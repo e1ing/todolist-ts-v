c20b5357fec87c013e294be502ee9632ceba6ca3
e1ing <elvira.kisling@gmail.com>
2021-05-10 19:42:21 +0300

    Updates

---
+++ precache-manifest.3ff8deed141b3890767bd1ba326afeaf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c15a4f94eb4c02de22dc6e20510485b",
+    "url": "/todolist-ts-videos/index.html"
+  },
+  {
+    "revision": "f97c89d58aa8e3a06f33",
+    "url": "/todolist-ts-videos/static/css/main.e701617a.chunk.css"
+  },
+  {
+    "revision": "4e0044aabe0d0c4638a3",
+    "url": "/todolist-ts-videos/static/js/2.f19a5572.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/todolist-ts-videos/static/js/2.f19a5572.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f97c89d58aa8e3a06f33",
+    "url": "/todolist-ts-videos/static/js/main.87378c8a.chunk.js"
+  },
+  {
+    "revision": "c6de12df1a7dcbfcea9c",
+    "url": "/todolist-ts-videos/static/js/runtime-main.46f240f5.js"
+  }
+]);
